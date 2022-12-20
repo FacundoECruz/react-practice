80bbfeed5e6eac877cdc417f878b50a2504d3215
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-20 14:30:08 -0300

    components moved to a single file

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
-import Input from './components/Input'
+import List from './components/List'
+
+function Input() {
+  return (
+    <div className="input-form">
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input id="name"></input>
+        <div className="input-button">
+            <button>Add Name</button>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 function App() {
   return <div className="App">
     <h1>React Practice</h1>
     <Input />
+    <List />
   </div>;
 }
 
