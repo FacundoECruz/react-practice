53ed8b5e3e03ee3cc0712878b8660054d991a273
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-23 20:02:58 -0300

    created table

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,27 +1,43 @@
 import React from "react";
 import "./App.css";
 
-function Button({ onClick }) {
-  const [clicks, setClicks] = React.useState(0);
-
-  function handleClick() {
-    setClicks(clicks + 1);
-  }
-
-  function handleReset() {
-    setClicks(0);
-  }
-
+function Table() {
   return (
-    <div className="container">
-      <button onClick={handleClick}>{clicks}</button>
-      <button onClick={handleReset}>Reset</button>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
 function App() {
-  return <Button />;
+  return (
+    <div className="container">
+      <Table />
+    </div>
+  );
 }
 
 export default App;
