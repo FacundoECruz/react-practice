325751ebc6f94d35b25d0085241737f038a1391d
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-23 20:05:25 -0300

    splited table

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,43 +1,47 @@
 import React from "react";
 import "./App.css";
 
-function Table() {
+const TableHeader = () => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  )
+}
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+      <tr>
+        <td>Dee</td>
+        <td>Aspiring actress</td>
+      </tr>
+      <tr>
+        <td>Dennis</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  )
 }
 
 function App() {
   return (
     <div className="container">
-      <Table />
+      <TableHeader />
+      <TableBody />
     </div>
-  );
+  ); 
 }
 
 export default App;
