a5b7aedefda187f51239d9adeab27e3ffaea603c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-22 22:46:18 -0300

    solved state and button exercise

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,36 +1,23 @@
 import React from "react";
 import "./App.css";
 
-function Greeting({ name, city, age }) {
-  const [club, setClub] = React.useState("");
+function Button({onClick}) {
 
-  const randomNum = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-  };
+  const [clicks, setClicks] = React.useState(0)
 
-  const handleClick = () => {
-    const options = ["Defensa y justicia", "Gimnasia"];
-    const nro = randomNum(0, options.length);
-    const result = options[nro];
-    setClub(result);
-  };
+  function handleClick(event) {
+    setClicks(clicks + 1)
+  }
 
-  return (
-    <div className="greeting">
-      <h1>Hola {name}</h1>
-      <h3>
-        Porque a los {age}, siendo de {city}
-      </h3>
-      <h2>Sos de {club}</h2>
-      <button onClick={handleClick}>Club</button>
-    </div>
-  );
+  return(
+    <button onClick={handleClick}>{clicks}</button>
+  )
 }
 
 function App() {
-  return <Greeting name="Antone" city="Rio Grande" age="27" />;
+  return (
+    <Button />
+  )
 }
 
 export default App;
