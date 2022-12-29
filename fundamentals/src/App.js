8820f5a8eb8f2ef11d2b1b4cb9a0a14d0c1835cf
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-29 16:42:51 -0300

    created round component

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,50 +1,9 @@
 import React from "react";
 import "./App.css";
-
-function Child() {
-
-  const [count, setCount] = React.useState(() => {return 0})
-
-  return(
-    <button onClick={() => setCount(previousCount => previousCount + 1)}>
-      {count}
-    </button>
-  )
-}
-
-function Element({showChild, updater}) {
-  return (
-    <div>
-      <form>
-        <label htmlFor="show">Show Child</label>
-        <input 
-        id="show" 
-        type="checkbox" 
-        checked={showChild}
-        onChange={e => updater(e.target.checked)} />
-      </form>
-      <div
-        style={{
-          padding: 10,
-          margin: 10,
-          height: 50,
-          width: 50,
-          border: "solid",
-        }}
-      >
-        {showChild ? <Child /> : null}
-      </div>
-    </div>
-  );
-}
+import Round from "./components/Round.js";
 
 function App() {
-
-  const [showChild, setShowChild] = React.useState(() => {
-    return false;
-  });
-
-  return <Element showChild={showChild} updater={setShowChild}/>;
+  return <Round />;
 }
 
 export default App;
---
+++ fundamentals/src/components/Round.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Round() {
+
+  const [round, setRound] = React.useState(1)
+  const [cartas, setCartas] = React.useState(0)
+
+  return (
+    <div className="round-container">
+      <h2>Ronda {round}</h2>
+      <h3>Cartas</h3>
+
+    </div>
+  );
+}
+
+export default Round;
