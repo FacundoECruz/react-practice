c419c81cfe7b644a5e766b13c1510aee9d2e11d4
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-29 18:53:28 -0300

    setted controls

--- fundamentals/src/components/PlayerGrid.js
+++ fundamentals/src/components/PlayerGrid.js
@@ -2,25 +2,42 @@ import React from "react";
 
 function PlayerGrid({ name }) {
 
-  const [bet, setBet] = React.useState(0)
-  const [lose, setLose] = React.useState(0)
+  const [control, setControl] = React.useState({
+    bet: 0,
+    lose: 0
+  })
   
+  const handleControl = (item, action) => {
+    
+    setControl((prevState) => {
+      return {
+        ...prevState,
+
+        [item]: action === 'up'
+        ? control[item] + 1
+        : control[item] - 1
+      }
+    })
+  } 
+
   return (
     <div className="player-info-container">
       <h1>{name}</h1>
       <div className="bet">
         <div className="bet-buttons">
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => handleControl('bet', 'up')}>+</button>
+          <button onClick={() => handleControl('bet', 'down')}>-</button>
+          <span>Apuesta</span>
         </div>
-        <div className="bet-display">{bet}</div>
+        <div className="bet-display">{control.bet}</div>
       </div>
       <div className="lose">
         <div className="lose-buttons">
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => handleControl('lose', 'up')}>+</button>
+          <button onClick={() => handleControl('lose', 'down')}>-</button>
+          <span>Pierde</span>
         </div>
-        <div className="lose-display">{lose}</div>
+        <div className="lose-display">{control.lose}</div>
       </div>
     </div>
   );
