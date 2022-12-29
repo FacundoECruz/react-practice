52226d90bf5c37f204436dabe688b4726ae180b2
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-29 17:53:08 -0300

    setted cards in current round

--- fundamentals/src/components/Round.js
+++ fundamentals/src/components/Round.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 function Round() {
-  const cardsPerRound = window.localStorage.getItem('cards')
-
-  console.log(cardsPerRound)
+  const cardsPerRound = JSON.parse(window.localStorage.getItem("cards"))
+  const players = JSON.parse(window.localStorage.getItem("players"))
 
   const [round, setRound] = React.useState(1);
-  const [cardsInCurrent, setCardsInCurrent] = React.useState(0);
+  const [cardsInCurrent, setCardsInCurrent] = React.useState(cardsPerRound[0]);
+
+  console.log(cardsInCurrent)
 
   return (
     <div className="round-container">
       <h2>Ronda {round}</h2>
-      <h3>Cartas</h3>
+      <h3>Cartas: {cardsInCurrent}</h3>
     </div>
   );
 }
