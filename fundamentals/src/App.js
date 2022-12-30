fa6e70fea300348e00da246adc636282dba685ca
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-30 13:35:58 -0300

    created table component

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import Round from "./components/Round.js";
+import Table from "./components/Table";
 
 function App() {
-  return <Round />;
+  return (
+    <div className="main-container">
+      <Round />
+      <Table />
+    </div>
+  );
 }
 
 export default App;
---
+++ fundamentals/src/components/Table.js
@@ -0,0 +1,7 @@
+function Table() {
+    return (
+        <h1 style={{color: 'blue'}}>Tabla de posiciones</h1>
+    )
+}
+
+export default Table;
