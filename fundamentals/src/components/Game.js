a483a1ba88ee5b96791270bf31f2db12d1b13be6
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-01-04 14:15:14 -0300

    using switch

--- fundamentals/src/components/Game.js
+++ fundamentals/src/components/Game.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "../stylesheets/Game.css";
 
-const reduceCount = (count, change) => ({...count, ...change})
+function reduceCount(state, action) {
+  switch(action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      throw new Error (`Unsupported ${action.type}`)
+    }
+  }
+}
 
-function Game({initialValue = 0, step = 2}) {
-
-  const [state, setState] = React.useReducer(reduceCount, {
+function Game({ initialValue = 0, step = 2 }) {
+  const [state, dispatch] = React.useReducer(reduceCount, {
     count: initialValue,
-  })
-  const {count} = state 
-  const increment = () => setState({count: count + step})
+  });
+  const { count } = state;
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>;
 }
