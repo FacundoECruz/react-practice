982be7bcc6580a3d67107fd04c93941f739e6fa8
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-01-09 12:38:39 -0300

    [wip] synchronized inputs with app state

--- fundamentals/src/components/Game.js
+++ fundamentals/src/components/Game.js
@@ -2,20 +2,42 @@ import React from "react";
 import "../stylesheets/Game.css";
 import players from "../javascripts/players";
 
+function gameStateReducer(state, action) {
+  switch (action.manage) {
+    case "bet": {
+      const player = state.game[action.player];
+      player.bet = action.bet;
+      return { ...state, ...player };
+    }
+    case "lost": {
+      const player = state.game[action.player];
+      player.betsLost = action.betsLost;
+      return { ...state, ...player };
+    }
+    default: {
+      throw new Error(`Unsupported action ${action.manage}`);
+    }
+  }
+}
 
 function Game() {
-
-  const [gameState, setGameState] = React.useState(players)
+  const [gameState, setGameState] = React.useReducer(gameStateReducer, {
+    game: players,
+  });
 
   const handleBetChange = (e) => {
-    console.log(e.target.value)
-  }
-  
+    setGameState({ manage: "bet", player: e.target.id, bet: e.target.value });
+    console.log(gameState)
+  };
+
   const handleLostChange = (e) => {
-    console.log(e.target.value)
-  }
-
-
+    setGameState({
+      manage: "lost",
+      player: e.target.id,
+      betsLost: e.target.value,
+    });
+    console.log(gameState)
+  };
 
   return (
     <div className="main-container">
@@ -25,10 +47,10 @@ function Game() {
           return (
             <div className="player-display-container">
               <h2>{p.name}</h2>
-              <label>Apuesta</label>
-              <input onChange={handleBetChange}/>
-              <label>Pierde</label>
-              <input onChange={handleLostChange}/>
+              <label htmlFor={p.key}>Apuesta</label>
+              <input id={p.key} value={p.bet} onChange={handleBetChange} />
+              <label htmlFor={p.key}>Pierde</label>
+              <input id={p.key} value={p.betsLost} onChange={handleLostChange} />
             </div>
           );
         })}
--- fundamentals/src/javascripts/players.js
+++ fundamentals/src/javascripts/players.js
@@ -1,20 +1,36 @@
 const players = [
-    {
-      name: "Antone",
-      score: 0,
-    },
-    {
-      name: "Facu",
-      score: 0,
-    },
-    {
-      name: "Chaky",
-      score: 0,
-    },
-    {
-      name: "Kevin",
-      score: 0,
-    },
-  ];
+  {
+    name: "Antone",
+    score: 0,
+    key: 0,
+    bet: 0,
+    win: false,
+    betsLost: 0,
+  },
+  {
+    name: "Facu",
+    score: 0,
+    key: 1,
+    bet: 0,
+    win: false,
+    betsLost: 0,
+  },
+  {
+    name: "Chaky",
+    score: 0,
+    key: 2,
+    bet: 0,
+    win: false,
+    betsLost: 0,
+  },
+  {
+    name: "Kevin",
+    score: 0,
+    key: 3,
+    bet: 0,
+    win: false,
+    betsLost: 0,
+  },
+];
 
 export default players;
