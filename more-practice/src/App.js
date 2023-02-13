8102dc66a04a16d7f1eacf588cc649f48344653c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-13 18:51:57 -0300

    compound component exercise

--- more-practice/src/App.js
+++ more-practice/src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
+  
+  const [loading, setLoading] = React.useState(false)
+  
+  const userData = {
+    name: 'Jorge',
+    role: 'admin',
+  }
+
+  function If ({ predicate, children }) {
+    return React.Children.map(children, child => {
+      return React.cloneElement(child, {predicate})
+    })
+  }
+
+  function Then ({predicate, children}) {
+    return predicate ? children : null
+  }
+  
+  function Else ({predicate, children}) {
+    return predicate ? null : children
+  }
+
+  return( 
     <div className="App">
-      <h1>More Practice</h1>
+      <If predicate={loading}>
+        <Then>Loading</Then>
+        <Else>{userData.name}</Else>
+      </If>
     </div>
-  );
+  )
 }
+
 
 export default App;
