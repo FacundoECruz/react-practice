a468da4cb463497318ef2b3e5b878d2b33e9e5b4
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-16 15:24:09 -0300

    displaying score and history

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./App.css";
-import Game from "./components/Game";
+import table from "./javascripts/table";
 
 function App() {
+
+  
+
   return (
-      <Game />
+    <div className="wrapper">
+      <div className="accordion">
+        {table.map((p, i) => {
+          return <div className="item">
+            <div className="name">
+              <h2>{p.name} {p.score}</h2>
+            </div>
+            <div className="history">{p.history}</div>
+          </div>;
+        })}
+      </div>
+    </div>
   );
 }
 
---
+++ fundamentals/src/javascripts/table.js
@@ -0,0 +1,24 @@
+const table = [
+  {
+    name: "Cantona",
+    score: 24,
+    history: [5, 6, 6, 8, -1]
+  },
+  {
+    name: "Platini",
+    score: 28,
+    history: [5, 6, 5, 5, 7]
+  },
+  {
+    name: "Ronaldo",
+    score: 17,
+    history: [-1, 6, -1, 8, 5]
+  },
+  {
+    name: "Klose",
+    score: 16,
+    history: [5, -1, 5, -1, 8]
+  },
+];
+
+export default table;
