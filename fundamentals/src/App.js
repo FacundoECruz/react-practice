d4f0cd3a13b6adfa084cc4278ac091a6e54f9b4e
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-16 15:42:07 -0300

    separated history numbers

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -3,27 +3,34 @@ import "./App.css";
 import table from "./javascripts/table";
 
 function App() {
-
-  const [selected, setSelected] = React.useState(null)
+  const [selected, setSelected] = React.useState(null);
 
   const toggle = (i) => {
-    if(selected === i){
-      return setSelected(null)
+    if (selected === i) {
+      return setSelected(null);
     }
-    setSelected(i)
-  }
+    setSelected(i);
+  };
 
   return (
     <div className="wrapper">
       <div className="accordion">
         {table.map((p, i) => {
-          return <div className="item">
-            <div className="name" onClick={() => toggle(i)}>
-              <p key={p.name}>{p.name} {p.score}</p>
-              <span>{selected === i ? "-" : "+"}</span>
+          return (
+            <div className="item">
+              <div className="name" onClick={() => toggle(i)}>
+                <p key={p.name}>
+                  {p.name} {p.score}
+                </p>
+                <span>{selected === i ? "-" : "+"}</span>
+              </div>
+              <div className={selected === i ? "history-show" : "history"}>
+                {p.history.map((n) => {
+                  return <p>{n}</p>
+                })}
+              </div>
             </div>
-            <div className={selected === i ? "history-show" : "history"}>{p.history}</div>
-          </div>;
+          );
         })}
       </div>
     </div>
