6995f803be97179e91bc7beebec5e01e6081d102
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-22 20:21:12 -0300

    solved consume jokes api exercise

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import useFetch from "./hooks/useFetch";
 
 function App() {
-  return <div className="App"></div>;
+
+  const { data, loading, error } = useFetch("https://v2.jokeapi.dev/joke/Any");
+
+  if (loading) return <h1> LOADING...</h1>;
+
+  if (error) console.log(error);
+
+  return <div className="App">
+    <h1>{data?.setup} : {data?.delivery}</h1>
+  </div>;
 }
 
 export default App;
