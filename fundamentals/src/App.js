3f1b6aff74b6123e73a2acc4d679abb0844cb50c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-22 19:45:00 -0300

    created useFetch hook

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -2,54 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-
-  function Form() {
-
-    const [username, setUsername] = React.useState("")
-
-    const handleSubmit = event => {
-      event.preventDefault()
-      const data = {
-        username: event.target.elements.username.value,
-        email: event.target.elements.email.value,
-        password: event.target.elements.password.value,
-      }
-      console.log(data)
-    }
-    
-    const capitalize = str => {
-      const capitalized = str.charAt(0).toUpperCase() + str.slice(1)
-      console.log(capitalized)
-      return capitalized
-    }
-
-    const handleChange = event => {
-      const {value} = event.target
-      setUsername(capitalize(value))
-    }
-
-    return(
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input value={username} id="username" type="text" onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input id="email" type="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input id="password" type="password" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    )
-  }
-
-  return <div className="App">
-    <Form />
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
