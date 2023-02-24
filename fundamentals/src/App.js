e3e3ae589a1a0d9b1fc7ca7883e89145fff8443c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-23 22:00:34 -0300

    added input render

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import Greeting from "./components/testFunction";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Greeting />
+  </div>;
 }
 
 export default App;
--- fundamentals/src/components/testFunction.js
+++ fundamentals/src/components/testFunction.js
@@ -1,12 +1,27 @@
+import{useState, useRef} from 'react'
+
 export function capitalize(name) {
   const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
   return capitalizedName;
 }
 
 function Greeting() {
-  return(
-    <h1>Hola</h1>
-  )
+
+  const [name, setName] = useState('')
+  const inputRef = useRef()
+
+  function handleChange() {
+    const inputText = capitalize(inputRef.current.value)
+    setName(inputText)
+  }
+
+  return (
+    <div>
+      <label>Nombre: </label>
+      <input ref={inputRef} id="name" type="text" value={name} onChange={handleChange}/>
+      <h1>Hola {name}</h1>
+    </div>
+  );
 }
 
-export default Greeting
+export default Greeting;
