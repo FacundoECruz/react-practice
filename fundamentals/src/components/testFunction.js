745ec203e3455401075ed116096112163efa5ad7
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-23 21:34:52 -0300

    created capitalize tests

---
+++ fundamentals/src/components/testFunction.js
@@ -0,0 +1,12 @@
+export function capitalize(name) {
+  
+  return 
+}
+
+function Greeting() {
+  return(
+    <h1>Hola</h1>
+  )
+}
+
+export default Greeting
---
+++ fundamentals/src/tests/Greeting.test.js
@@ -0,0 +1,13 @@
+import { capitalize } from "../components/testFunction"
+
+describe('funciones del componente Greeting', () => {
+    describe('capitalize', () => {
+        test('debe retornar string vacio si recibe un string vacio', () => {
+            expect(capitalize('')).toBe('');
+        });
+
+        test('debe retornar el string capitalizado', () => {
+            expect(capitalize('antone')).toBe('Antone');
+        });
+    })
+})
