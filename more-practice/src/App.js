5af0cede562672882dd6869cb2c3cd2290175773
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-09 15:43:01 -0300

    cleaned for testing exercises

--- more-practice/src/App.js
+++ more-practice/src/App.js
@@ -1,30 +1,8 @@
 import "./App.css";
-import React from "react";
-import Names from "./components/Names";
-import History from "./components/History";
-import Table from "./components/Table";
-import Control from "./components/Control";
+import {useState, useEffect} from "react";
 
 function App() {
 
-  const [display, setDisplay] = React.useState(true)
-
-  function handleCheck() {
-    setDisplay(!display)
-  }
-
-  React.useEffect(() => {
-    console.log(display)
-  })
-
-  return (
-    <Table predicate={display}>
-      <h1>Table</h1>
-      <Control />
-      <Names />
-      <History />
-    </Table>
-  );
 }
 
 export default App;
--- more-practice/src/components/Table.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Table({predicate, children }) {
-return React.Children.map(children, child => {
-        return React.cloneElement(child, {predicate})
-      })
-    }
-
-export default Table;
