8b8c8af11ae6fef0fb71bf28241a3633d81d2e62
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-14 20:26:43 -0300

    accordion added

--- fundamentals/src/components/Posts.js
+++ fundamentals/src/components/Posts.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 function Post() {
   const [post, setPost] = useState([]);
+  const [selected, setSelected] = useState(null);
+
 
   const apiGet = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -12,14 +14,27 @@ function Post() {
       });
   };
 
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+    setSelected(i);
+  };
+
   useEffect(() => {
     apiGet();
   }, []);
 
-  return post.map((d) => {
+  return post.map((d, i) => {
     return (
-      <div className="post-container" key={d.id}>
-        <h3>{d.title}</h3>
+      <div className="post-container" key={d.id} onClick={() => toggle(i)}>
+        <h3>
+          {d.title}
+          <span id="user-id">{d.userId}</span>
+        </h3>
+        <div className={selected === i ? "content-show" : "content"}>
+          {d.body}          
+        </div>
       </div>
     );
   });
