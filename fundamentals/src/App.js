2dacaa6f38f90325500eb3bf1bb6d09a291a7b21
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-14 18:52:11 -0300

    moved list to the left side

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
-import Data from "./components/Data";
+import Data from "./components/Posts";
+import './stylesheets/posts.css'
 
 function App() {
-  return <div className="App">
-    <h1>Data</h1>
-    <Data />
-  </div>;
+  return (
+    <div className="App">
+      <h1>SocioloGists</h1>
+      <div className="posts-list-container">
+        <Data />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- fundamentals/src/components/Posts.js
+++ fundamentals/src/components/Posts.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 
-function Data() {
-  const [data, setData] = useState([]);
+function Post() {
+  const [post, setPost] = useState([]);
 
   const apiGet = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        setData(json);
+        setPost(json);
       });
   };
 
@@ -16,17 +16,13 @@ function Data() {
     apiGet();
   }, []);
 
-  return data.map((d) => {
-    return(
-    <>
-      <h1>
-        {d.title} 
-      </h1>
-      <p>from {d.userId}</p>
-      <p>{d.body}</p>
-    </>
+  return post.map((d) => {
+    return (
+      <div className="post-container" key={d.id}>
+        <h2>{d.title}</h2>
+      </div>
     );
   });
 }
 
-export default Data;
+export default Post;
